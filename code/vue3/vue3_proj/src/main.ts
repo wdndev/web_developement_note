ca54040c32bde742611e943f23c8268c6674e7c0
wdndev <dongnian.wang@outlook.com>
2025-06-14 15:31:42 +0800

    通信组件

--- code/vue3/vue3_proj/src/main.ts
+++ code/vue3/vue3_proj/src/main.ts
@@ -1,16 +1,8 @@
-// 引入 createApp 用于创建应用
-import { createApp } from "vue";
-// 引入 App 根组件
-import App from "./App.vue"
-// 引入pinia
-import { createPinia } from "pinia";
+import {createApp} from 'vue'
+import App from './App.vue'
 
-// 创建app
+// 创建应用
 const app = createApp(App)
-// 创建pinia
-const pinia = createPinia()
-// 安装pinia
-app.use(pinia)
 
-// 挂载整个应用到app容器中
-app.mount("#app");
+// 挂载应用
+app.mount('#app')
